30a58e605ee002607dbb5447df10ead962e42a50
MaraCmara <57595792+MaraCmara@users.noreply.github.com>
2019-11-10 19:51:59 +0100

    Add files via upload

--- src/data/articles.js
+++ src/data/articles.js
@@ -1,268 +1,268 @@
 const articles = [
   {
     name: "Hong Kong",
-    image: "/IMG/23/01.png",
+    image: "/IMG/23/01.jpg",
     description: "Plakát k výstavě týkající se Hong Kongských protestů, kterou pořádá Artrafika. Tato ilustrace vyjadřuje vztah mezi Čínou a Hong Kongem za použití známých atributů protestů, jako je například žlutý deštník.",
     resources: [
       {
-        image: "/IMG/23/01.png",
+        image: "/IMG/23/01.jpg",
       },
     ],
   }, {
     name: "Nový věk",
-    image: "/IMG/22/01.png",
+    image: "/IMG/22/01.jpg",
     description: "Editorial ilustrace na téma vztahu lidstva k digitálním médiím",
     resources: [
       {
-        image: "/IMG/22/01.png",
+        image: "/IMG/22/01.jpg",
       },
     ],
   }, {
     name: "Sametová revoluce",
-    image: "/IMG/21/01.png",
+    image: "/IMG/21/01.jpg",
     description: "Plakát vytvořený pro Skautský institut v Praze k příležitosti 30. výročí Sametové revoluce",
     resources: [
       {
-        image: "/IMG/20/01.png",
+        image: "/IMG/20/01.jpg",
       },
     ],
   },
   {
     name: "Pán Much",
-    image: "/IMG/20/01.png",
+    image: "/IMG/20/01.jpg",
     description: "Obálka knihy Pán Much od Williama Goldinga",
     resources: [
       {
-        image: "/IMG/20/01.png",
+        image: "/IMG/20/01.jpg",
       },
     ],
   },
   {
     name: "Bez Vajglu",
-    image: "/IMG/19/nahled.png",
+    image: "/IMG/19/nahled.jpg",
     description: "Ilustrace pro Bezvajglu.",
     resources: [
       {
-        image: "/IMG/19/01.png",
+        image: "/IMG/19/01.jpg",
       },
       {
-        image: "/IMG/19/02.png",
+        image: "/IMG/19/02.jpg",
       },
       {
-        image: "/IMG/19/03.png",
+        image: "/IMG/19/03.jpg",
       },
     ],
   },
   {
     name: "Válka",
-    image: "/IMG/18/nahled.png",
+    image: "/IMG/18/nahled.jpg",
     description: "Editorial na téma „Válka“",
     resources: [
       {
-        image: "/IMG/18/01.png",
+        image: "/IMG/18/01.jpg",
       },
       {
-        image: "/IMG/18/02.png",
+        image: "/IMG/18/02.jpg",
       },
     ],
   },
   {
     name: "Mural Vltavská",
-    image: "/IMG/17/nahled.png",
+    image: "/IMG/17/nahled.jpg",
     description: "Návrh mural artu pro Prahu 7 na stanici metra Vltavská",
     resources: [
       {
-        image: "/IMG/17/01.png",
+        image: "/IMG/17/01.jpg",
       },
       {
-        image: "/IMG/17/02.png",
+        image: "/IMG/17/02.jpg",
       },
       {
-        image: "/IMG/17/03.png",
+        image: "/IMG/17/03.jpg",
       },
     ],
   },
  
   {
     name: "Propojení",
-    image: "/IMG/16/01.png",
+    image: "/IMG/16/01.jpg",
     description: "",
     resources: [
       {
-        image: "/IMG/16/01.png",
+        image: "/IMG/16/01.jpg",
       },
     ],
   }, {
     name: "Sbohem Andy!",
-    image: "/IMG/15/01.png",
+    image: "/IMG/15/01.jpg",
     description: "Ilustrace k uctění dvou velkých umělců o vztahu Andyho Warhola a Jean Michele Basquiata.",
     resources: [
       {
-        image: "/IMG/15/01.png",
+        image: "/IMG/15/01.jpg",
       },
     ],
   },
   {
     name: "Odpad",
-    image: "/IMG/14/01.png",
+    image: "/IMG/14/01.jpg",
     description: "Co po sobě zanechali dávné civilizace a co tu zůstane po nás? Osobní projekt, který se zabývá současnými problémy.",
     resources: [
       {
-        image: "/IMG/14/01.png",
+        image: "/IMG/14/01.jpg",
       },
     ],
   },
   {
     name: "Colors of Ostrava",
-    image: "/IMG/13/nahled.png",
+    image: "/IMG/13/nahled.jpg",
     description: "Vizuál pro hudební festival Colors of Ostrava",
     resources: [
       {
-        image: "/IMG/13/01.png",
+        image: "/IMG/13/01.jpg",
       },
       {
-        image: "/IMG/13/02.png",
+        image: "/IMG/13/02.jpg",
       },
       {
-        image: "/IMG/13/03.png",
+        image: "/IMG/13/03.jpg",
       },
     ],
   },
   {
     name: "Editorial",
-    image: "/IMG/12/01.png",
+    image: "/IMG/12/01.jpg",
     description: "Ilustrace o vztahu velkých firem k přírodě, které na ni neberou ohled.",
     resources: [
       {
-        image: "/IMG/12/01.png",
+        image: "/IMG/12/01.jpg",
       },
     ],
   },
   {
     name: "Multi-Love",
-    image: "/IMG/11/01.png",
+    image: "/IMG/11/01.jpg",
     description: "Alternativní obal alba Multi-Love kapely Unknown Mortal Orchestra",
     resources: [
       {
-        image: "/IMG/11/01.png",
+        image: "/IMG/11/01.jpg",
       },
     ],
   },
   {
     name: "Moře věcí",
-    image: "/IMG/10/01.png",
+    image: "/IMG/10/01.jpg",
     description: "Ilustrace na téma minimalismus",
     resources: [
       {
-        image: "/IMG/10/01.png",
+        image: "/IMG/10/01.jpg",
       },
     ],
   },
   {
     name: "Lidé a příroda",
-    image: "/IMG/09/01.png",
+    image: "/IMG/09/01.jpg",
     description: "Editorial na téma „Lidé a příroda“",
     resources: [
       {
-        image: "/IMG/09/01.png",
+        image: "/IMG/09/01.jpg",
       },
     ],
   },
   {
     name: "Half • Alive",
-    image: "/IMG/08/nahled.png",
+    image: "/IMG/08/nahled.jpg",
     description: "Ilustrace pro hudební skupinu Half • Alive",
     resources: [
       {
-        image: "/IMG/08/01.png",
+        image: "/IMG/08/01.jpg",
       },
       {
-        image: "/IMG/08/02.png",
+        image: "/IMG/08/02.jpg",
       },
       {
-        image: "/IMG/08/03.png",
+        image: "/IMG/08/03.jpg",
       },
     ],
   },
   {
     name: "Auta",
-    image: "/IMG/07/01.png",
+    image: "/IMG/07/01.jpg",
     description: "Volná ilustrace",
     resources: [
       {
-        image: "/IMG/07/01.png",
+        image: "/IMG/07/01.jpg",
       },
     ],
   },
   {
     name: "Atomová bomba",
-    image: "/IMG/06/nahled.png",
+    image: "/IMG/06/nahled.jpg",
     description: "Ilustrace na obálku magazínu RedWay",
     resources: [
       {
-        image: "/IMG/06/01.png",
+        image: "/IMG/06/01.jpg",
       },
       {
-        image: "/IMG/06/02.png",
+        image: "/IMG/06/02.jpg",
       },
     ],
   },
   {
     name: "Ohrožené druhy",
-    image: "/IMG/05/01.png",
+    image: "/IMG/05/01.jpg",
     description: "Ilustrace na téma „Ohrožené druhy“ pro komiksový festival KOMA v Brně",
     resources: [
       {
-        image: "/IMG/05/01.png",
+        image: "/IMG/05/01.jpg",
       },
     ],
   },
   {
     name: "Samota",
-    image: "/IMG/04/01.png",
+    image: "/IMG/04/01.jpg",
     description: "Volná ilustrace",
     resources: [
       {
-        image: "/IMG/04/01.png",
+        image: "/IMG/04/01.jpg",
       },
     ],
   },
   {
     name: "RedWay komiksy",
-    image: "/IMG/03/nahled.png",
+    image: "/IMG/03/nahled.jpg",
     description: "Komiksy pro magazín RedWay",
     resources: [
       {
-        image: "/IMG/03/01.png",
+        image: "/IMG/03/01.jpg",
       },
       {
-        image: "/IMG/03/02.png",
+        image: "/IMG/03/02.jpg",
       },
       {
-        image: "/IMG/03/03.png",
+        image: "/IMG/03/03.jpg",
       },
     ],
   },
   {
     name: "Rytíř",
-    image: "/IMG/02/01.png",
+    image: "/IMG/02/01.jpg",
     description: "Volná ilustrace",
     resources: [
       {
-        image: "/IMG/02/01.png",
+        image: "/IMG/02/01.jpg",
       },
     ],
   },
   {
     name: "Povaleč",
-    image: "/IMG/01/nahled.png",
+    image: "/IMG/01/nahled.jpg",
     description: "Ilustrace plakátu pro festival Povaleč 2018",
     resources: [
       {
-        image: "/IMG/01/01.png",
+        image: "/IMG/01/01.jpg",
       },
       {
-        image: "/IMG/01/02.png",
+        image: "/IMG/01/02.jpg",
       },
     ],
   },
--- src/data/articlesEn.js
+++ src/data/articlesEn.js
@@ -1,268 +1,268 @@
 const articles = [
   {
     name: "Hong Kong",
-    image: "/IMG/23/01.png",
+    image: "/IMG/23/01.jpg",
     description: "Poster for exhibition by artrafika gallery which was all about Hong Kong protests and the whole situation. In this illustration I express relationship between China and Hong Kong by famous attributes of protests for example yellow umbrella.",
     resources: [
       {
-        image: "/IMG/23/01.png",
+        image: "/IMG/23/01.jpg",
       },
     ],
   }, {
     name: "New age",
-    image: "/IMG/22/01.png",
+    image: "/IMG/22/01.jpg",
     description: "Editorial illustration about a relationship between mankind and digital media",
     resources: [
       {
-        image: "/IMG/22/01.png",
+        image: "/IMG/22/01.jpg",
       },
     ],
   }, {
     name: "Velvet revolution",
-    image: "/IMG/21/01.png",
+    image: "/IMG/21/01.jpg",
     description: "Poster for Scout institut in Prague for 30th anniversary o Velvet revolution",
     resources: [
       {
-        image: "/IMG/20/01.png",
+        image: "/IMG/20/01.jpg",
       },
     ],
   },
   {
     name: "Lord of Flies",
-    image: "/IMG/20/01.png",
+    image: "/IMG/20/01.jpg",
     description: "Book cover for Lord of Flies by William Golding",
     resources: [
       {
-        image: "/IMG/20/01.png",
+        image: "/IMG/20/01.jpg",
       },
     ],
   },
   {
     name: "Bez Vajglu",
-    image: "/IMG/19/nahled.png",
+    image: "/IMG/19/nahled.jpg",
     description: "Set of illustrations for Bezvajglu.cz",
     resources: [
       {
-        image: "/IMG/19/01.png",
+        image: "/IMG/19/01.jpg",
       },
       {
-        image: "/IMG/19/02.png",
+        image: "/IMG/19/02.jpg",
       },
       {
-        image: "/IMG/19/03.png",
+        image: "/IMG/19/03.jpg",
       },
     ],
   },
   {
     name: "War",
-    image: "/IMG/18/nahled.png",
+    image: "/IMG/18/nahled.jpg",
     description: "Editorial illustration on topic „War“",
     resources: [
       {
-        image: "/IMG/18/01.png",
+        image: "/IMG/18/01.jpg",
       },
       {
-        image: "/IMG/18/02.png",
+        image: "/IMG/18/02.jpg",
       },
     ],
   },
   {
     name: "Mural Vltavska",
-    image: "/IMG/17/nahled.png",
+    image: "/IMG/17/nahled.jpg",
     description: "Concept of mural art on underground station Vltavska",
     resources: [
       {
-        image: "/IMG/17/01.png",
+        image: "/IMG/17/01.jpg",
       },
       {
-        image: "/IMG/17/02.png",
+        image: "/IMG/17/02.jpg",
       },
       {
-        image: "/IMG/17/03.png",
+        image: "/IMG/17/03.jpg",
       },
     ],
   },
  
   {
     name: "Conection",
-    image: "/IMG/16/01.png",
+    image: "/IMG/16/01.jpg",
     description: "",
     resources: [
       {
-        image: "/IMG/16/01.png",
+        image: "/IMG/16/01.jpg",
       },
     ],
   }, {
     name: "Goodbye Andy!",
-    image: "/IMG/15/01.png",
+    image: "/IMG/15/01.jpg",
     description: "illustration to honor two great artist is about year 1987, the year when Andy Warhol died, which had a huge impact on Jean Michele Basquiat in the last year of his life, that ended up too early.",
     resources: [
       {
-        image: "/IMG/15/01.png",
+        image: "/IMG/15/01.jpg",
       },
     ],
   },
   {
     name: "Garbage",
-    image: "/IMG/14/01.png",
+    image: "/IMG/14/01.jpg",
     description: "What ancient civilizations leave behind them and what our civilization will leave, that’s the question. Personal Illustration about garbage problems of the contemporary world.",
     resources: [
       {
-        image: "/IMG/14/01.png",
+        image: "/IMG/14/01.jpg",
       },
     ],
   },
   {
     name: "Colors of Ostrava",
-    image: "/IMG/13/nahled.png",
+    image: "/IMG/13/nahled.jpg",
     description: "Illustration for music festival Colors of Ostrava",
     resources: [
       {
-        image: "/IMG/13/01.png",
+        image: "/IMG/13/01.jpg",
       },
       {
-        image: "/IMG/13/02.png",
+        image: "/IMG/13/02.jpg",
       },
       {
-        image: "/IMG/13/03.png",
+        image: "/IMG/13/03.jpg",
       },
     ],
   },
   {
     name: "Corporated nature",
-    image: "/IMG/12/01.png",
+    image: "/IMG/12/01.jpg",
     description: "Illustration about relationship between nature and corporate world that often doesn’t care about it.",
     resources: [
       {
-        image: "/IMG/12/01.png",
+        image: "/IMG/12/01.jpg",
       },
     ],
   },
   {
     name: "Multi-Love",
-    image: "/IMG/11/01.png",
+    image: "/IMG/11/01.jpg",
     description: "Alternative cover illustration for album Multi-Love by Unknown Mortal Orchestra",
     resources: [
       {
-        image: "/IMG/11/01.png",
+        image: "/IMG/11/01.jpg",
       },
     ],
   },
   {
     name: "The sea of things",
-    image: "/IMG/10/01.png",
+    image: "/IMG/10/01.jpg",
     description: "Illustration on topic of minimalism",
     resources: [
       {
-        image: "/IMG/10/01.png",
+        image: "/IMG/10/01.jpg",
       },
     ],
   },
   {
     name: "Humans and nature",
-    image: "/IMG/09/01.png",
+    image: "/IMG/09/01.jpg",
     description: "Editorial illustration on topic „Humans and nature“",
     resources: [
       {
-        image: "/IMG/09/01.png",
+        image: "/IMG/09/01.jpg",
       },
     ],
   },
   {
     name: "Half • Alive",
-    image: "/IMG/08/nahled.png",
+    image: "/IMG/08/nahled.jpg",
     description: "Set of illustrations for band Half • Alive",
     resources: [
       {
-        image: "/IMG/08/01.png",
+        image: "/IMG/08/01.jpg",
       },
       {
-        image: "/IMG/08/02.png",
+        image: "/IMG/08/02.jpg",
       },
       {
-        image: "/IMG/08/03.png",
+        image: "/IMG/08/03.jpg",
       },
     ],
   },
   {
     name: "Cars",
-    image: "/IMG/07/01.png",
+    image: "/IMG/07/01.jpg",
     description: "Personal illustration",
     resources: [
       {
-        image: "/IMG/07/01.png",
+        image: "/IMG/07/01.jpg",
       },
     ],
   },
   {
     name: "Nuclear bomb",
-    image: "/IMG/06/nahled.png",
+    image: "/IMG/06/nahled.jpg",
     description: "Cover illustration for RedWay magazine",
     resources: [
       {
-        image: "/IMG/06/01.png",
+        image: "/IMG/06/01.jpg",
       },
       {
-        image: "/IMG/06/02.png",
+        image: "/IMG/06/02.jpg",
       },
     ],
   },
   {
     name: "Endangered species",
-    image: "/IMG/05/01.png",
+    image: "/IMG/05/01.jpg",
     description: "Illustration on topic of „Endangered species“ for comics festival KOMA in Brno",
     resources: [
       {
-        image: "/IMG/05/01.png",
+        image: "/IMG/05/01.jpg",
       },
     ],
   },
   {
     name: "Loneliness",
-    image: "/IMG/04/01.png",
+    image: "/IMG/04/01.jpg",
     description: "Personal illustration",
     resources: [
       {
-        image: "/IMG/04/01.png",
+        image: "/IMG/04/01.jpg",
       },
     ],
   },
   {
     name: "RedWay comics",
-    image: "/IMG/03/nahled.png",
+    image: "/IMG/03/nahled.jpg",
     description: "Comics for RedWay magazine",
     resources: [
       {
-        image: "/IMG/03/01.png",
+        image: "/IMG/03/01.jpg",
       },
       {
-        image: "/IMG/03/02.png",
+        image: "/IMG/03/02.jpg",
       },
       {
-        image: "/IMG/03/03.png",
+        image: "/IMG/03/03.jpg",
       },
     ],
   },
   {
     name: "The knight",
-    image: "/IMG/02/01.png",
+    image: "/IMG/02/01.jpg",
     description: "Personal illustration",
     resources: [
       {
-        image: "/IMG/02/01.png",
+        image: "/IMG/02/01.jpg",
       },
     ],
   },
   {
     name: "Povaleč",
-    image: "/IMG/01/nahled.png",
+    image: "/IMG/01/nahled.jpg",
     description: "Illustration of poster for Povaleč festival 2018",
     resources: [
       {
-        image: "/IMG/01/01.png",
+        image: "/IMG/01/01.jpg",
       },
       {
-        image: "/IMG/01/02.png",
+        image: "/IMG/01/02.jpg",
       },
     ],
   },
